e82a1c74c18cda800f725c67143f3bf37235aa2a
MOR-E <pedrohenrique88977944@gmail.com>
2023-03-09 20:07:08 -0300

    conexao com o banco de dados feito

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 const express = require("express");
 const app = express();
+const connection = require("./database/database");
+const bodyParser = require("body-parser");
 
+//View Engine
 app.set('view engine', 'ejs');
+
+//Static    
 app.use(express.static('public'));
+
+
+//Body parser
+app.use(bodyParser.urlencoded({extends: false}));
+app.use(bodyParser.json());
+
+//database
+connection
+    .authenticate()
+    .then(() => {
+        console.log("Conexão feita com sucesso");
+    }).catch((error) => {
+        console.log(error)
+    })
+
 
 app.get("/", (req, res) => {
     res.render("start/login")
 });  
+
+
 
 app.listen(8001, ()=>{
     console.log("aplicação rodando!")
